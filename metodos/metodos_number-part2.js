46a59768207919a6914d9cd1c3235ae7192b7a74
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-05-29 18:17:01 -0300

    parse int

---
+++ metodos/metodos_number-part2.js
@@ -0,0 +1,6 @@
+let flutuanteString = '32.7869';
+let inteiroString = '42'; 
+
+// Converter o valor (geralmente string) para um número
+console.log(Number.parseFloat(flutuanteString));
+console.log(Number.parseInt(inteiroString));
